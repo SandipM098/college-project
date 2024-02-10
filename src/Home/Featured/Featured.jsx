0374469f2ef6140cf70b251ba766b9621054212d
Trickster <reone054@gmail.com>
2024-02-10 23:28:52 +0545

    feature half

---
+++ src/Home/Featured/Featured.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
+
+export default function Featured() {
+  const Featured = () => {
+    const carsData = [
+      {
+        id: 0,
+        img: "/src/assets/image/car1.png",
+        name: "Toyota Car",
+        price: "30,000",
+      },
+      {
+        id: 1,
+        img: "/src/assets/image/car1.png",
+        name: "Toyota Car",
+        price: "30,000",
+      },
+      {
+        id: 2,
+        img: "/src/assets/image/car1.png",
+        name: "Toyota Car",
+        price: "30,000",
+      },
+      {
+        id: 3,
+        img: "/src/assets/image/car1.png",
+        name: "Toyota Car",
+        price: "30,000",
+      },
+      {
+        id: 4,
+        img: "/src/assets/image/car1.png",
+        name: "Toyota Car",
+        price: "30,000",
+      },
+      {
+        id: 5,
+        img: "/src/assets/image/car1.png",
+        name: "Toyota Car",
+        price: "30,000",
+      },
+    ];
+  };
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidestoscroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplayspeed : 2000,
+    cssEase : "linear",
+    arrows : false, 
+    responsive : [
+      {
+        breakpoint : 1023,
+        settings:{
+          slidesToShow : 3,
+          slidestoscroll : 3,
+          infinite : true, 
+          dots: true,
+        }
+      },
+      {
+        breakpoint : 768,
+        settings:{
+          slidesToShow : 2,
+          slidestoscroll : 2,
+          initialSlide : 2,
+        }
+      },
+      {
+        breakpoint : 480,
+        settings:{
+          slidesToShow : 1,
+          slidestoscroll : 1,
+          initialSlide : 2,
+        }
+      },
+      
+    ]
+  };
+  return (
+    <div>
+      <h1 className="font-bold text-4xl text-center">
+        Feature <span className="text-blue-600">Cars</span>
+      </h1>
+
+      <p>Lorem ipsum dolor sit amet consectetur.</p>
+
+      <div></div>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../Home/Hero/Hero'
+import Feature from '../Home/Featured/Featured.jsx'
+
 
 export default function home(){
   return (
     <>
     <Navbar/>
     <Hero/>
+    <Feature/>
+
+    
     </>
     
   )
