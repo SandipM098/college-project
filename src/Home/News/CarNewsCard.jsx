be8492f40dc58ae5674b6e4a755c9ccd363bf1ac
Sandip Magar <reone054@gmail.com>
2025-04-08 12:45:47 +0545

    card gap

--- src/Home/News/CarNewsCard.jsx
+++ src/Home/News/CarNewsCard.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React from "react";
 
-function CarNewsCard({id,img, desc}) {
+function CarNewsCard({ img, desc }) {
   return (
-    
-
-    <div className='border-2 border-zinc-900 rounded-md cursor-pointer' key={id}>
-      <img src={img} alt = "img"/>
-      <h3 className='font-semibold text-lg p-2'>{desc}</h3>
+    <div className="mx-4 mb-8 group cursor-pointer">
+      <div
+        className="relative border-2 border-gray-800 rounded-xl overflow-hidden 
+                      bg-white transform transition-all duration-500 hover:-translate-y-2 
+                      hover:shadow-xl"
+      >
+        <div className="overflow-hidden">
+          <img
+            src={img}
+            alt={desc}
+            className="w-full h-56 object-cover transition-transform duration-700 
+                      group-hover:scale-105"
+          />
+        </div>
+        <div
+          className="p-5 bg-gradient-to-t from-gray-900 to-transparent absolute bottom-0 
+                       w-full text-white"
+        >
+          <h3
+            className="font-semibold text-lg leading-tight transition-all duration-300 
+                        group-hover:text-blue-300"
+          >
+            {desc}
+          </h3>
+        </div>
+      </div>
     </div>
-    
-  )
+  );
 }
-export default CarNewsCard
+
+export default CarNewsCard;
